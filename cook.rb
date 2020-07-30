eaec436996e5a88e54b5b5ea0df380f3af78916a
Yi Qing Sim <yiqing.sim@shopify.com>
2020-07-29 22:53:06 -0400

    Bump to v0.2.0.

--- cook.rb
+++ cook.rb
@@ -1,14 +1,14 @@
 class Cook < Formula
   desc "command line recipe management"
   homepage "https://github.com/ysim/cook"
-  url "https://github.com/ysim/cook/releases/download/v0.1.1/cook-darwin-v0.1.1.tar.gz"
-  sha256 "e11e651c14a4655508e6d55780980490acc131175baf4098ab63f99647bbd793"
+  url "https://github.com/ysim/cook/releases/download/v0.2.0/cook-darwin-v0.2.0.tar.gz"
+  sha256 "ae84f1ba8271f93b2d654024267832701dd10f19a6eb1889efa9c80044b32f68"
 
   bottle :unneeded
 
   resource "completion" do
-    url "https://github.com/ysim/cook/archive/v0.1.1.tar.gz"
-    sha256 "e29494e63aa9eafbd07b95c9c2b863b3145f85812e569739b9299591afcb536c"
+    url "https://github.com/ysim/cook/archive/v0.2.0.tar.gz"
+    sha256 "ccf5efca742ffcaf297807354a545d4af101227f5679d64ef2de09816866e2a4"
   end
 
   def install
